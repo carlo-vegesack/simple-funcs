992c54a16f6132f61c9cf2d7e9df0c76799bbb99
carlo-vegesack <carlo.vegesack@gmail.com>
2023-10-06 10:00:15 +0200

    test

--- src/test_funcs.py
+++ src/test_funcs.py
@@ -1,4 +1,4 @@
-# This directory will be checked with pytest. It will examine
+# This  directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
