e91da71b7fab5e42e18f1648633dd8f4e1fd27d4
carlo-vegesack <carlo.vegesack@gmail.com>
2023-10-06 09:51:16 +0200

    done

--- src/funcs.py
+++ src/funcs.py
@@ -1,43 +1,59 @@
 """Exercises with simple functions"""
-
-
+import doctest
+import math
 def prod(a, b, c):
     """
     Compute the product of three numbers.
 
     >>> prod(1, 2, 3)
-    'TEST ME!'
+    6
     """
+    p = a * b * c
+    return p
+
     ...
 
 
+a = 7
 def prod2(b):
     """
     Get a global a and write to a local c before computing prod(a, b, c)
 
     >>> prod2(42)
-    'TEST ME'
+    2058
     """
-    ...
+    c = 7
+    p2 = a * b * c
+    return p2
 
+    ...
 
 def longest(x, y):
     """
     Returning the longest of two lists.
 
     >>> longest([1, 2, 3], [4, 5])
-    'TEST ME'
+    [1, 2, 3]
     """
-    ...
+    if len(x) > len(y):
+        result = x
+    else:
+        result = y
+    return result
 
+
+    ...
 
 def dist(p1, p2):
     """
     Compute the distance between p1 and p2.
 
     >>> dist((1,2), (3,4))
-    'TEST ME'
+    2.8284271247461903
     """
     x1, y1 = p1
     x2, y2 = p2
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
+
     ...
